f60bcdbf45409635e290e7b3545788bfb80eeecf
Ramzan Ali <purvam98@gmail.com>
2018-04-21 08:20:00 -0400

    every saturday

---
+++ liri.js
@@ -0,0 +1,148 @@
+require("dotenv").config();
+var keyfile = require("./keys.js");
+//var spotify = new Spotify(keys.spotify);
+var Twitter = require('twitter');
+var client = new Twitter(keyfile.twitter);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keyfile.spotify);
+var request = require('request');
+var fs = require('fs');
+var allarg=process.argv;
+var final="";
+for(var m=3;m<allarg.length;m++)
+{
+    if(m>3 && m<allarg.length){
+        final = final + "+" + allarg[i];
+      } else{
+        final = final + allarg[i];
+      }
+}
+var arg = process.argv[2];
+
+if (arg === "my-tweets") {
+    var params = { screen_name: '@ChokshiPurvam' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log(tweets[i].text);
+                console.log("-------");
+            }
+        }
+    });
+
+}
+else if (arg === "spotify-this-song") {
+    if(process.argv[3])
+    {
+    spotify.search({ type: 'track', query: final }, function (err, data) {
+        if (!err) {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var songData = data.tracks.items[i];
+                //artist
+                console.log("Artist: " + songData.artists[0].name);
+                //song name
+                console.log("Song: " + songData.name);
+                //spotify preview link
+                console.log("Preview URL: " + songData.preview_url);
+                //album name
+                console.log("Album: " + songData.album.name);
+                console.log("-----------------------");
+
+
+            }
+        }
+    });
+    }
+    else
+    {
+        spotify.search({ type: 'track', query: "The+Sign" }, function (err, data) {
+            if (!err) {
+                for (var i = 0; i < data.tracks.items.length; i++) {
+                    var songData = data.tracks.items[i];
+                    //artist
+                    if(songData.artists[0].name==="Ace of Base"&&songData.name==="The Sign")
+                    {
+                    console.log("Artist: " + songData.artists[0].name);
+                    //song name
+                    console.log("Song: " + songData.name);
+                    //spotify preview link
+                    console.log("Preview URL: " + songData.preview_url);
+                    //album name
+                    console.log("Album: " + songData.album.name);
+                    console.log("-----------------------");
+                    }
+                    else{
+                        
+                    }
+    
+    
+                }
+            }
+        });
+    }
+
+}
+else if(arg==="movie-this")
+{
+    
+    if(process.argv[3])
+    {
+     
+        var omdbURL = "http://www.omdbapi.com/?t="+final+"&y=&plot=short&apikey=trilogy";
+    
+        request(omdbURL, function (error, response, body){
+          if(!error && response.statusCode == 200){
+            var body = JSON.parse(body);
+               
+            console.log("Title: " + body.Title);
+            console.log("Release Year: " + body.Year);
+            console.log("IMdB Rating: " + body.imdbRating);
+            console.log("Country: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+            console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+          }
+    });
+}
+else
+{
+    console.log("-----------------------");
+    console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+    console.log("It's on Netflix!");
+
+
+
+}
+
+}
+else if(arg==="do-what-it-says")
+{
+    fs.readFile('random.txt', "utf8", function(error, data){
+    
+    var text = data.split(',');
+    spotify.search({ type: 'track', query: text[1] }, function (err, data) {
+        if (!err) {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var songData = data.tracks.items[i];
+                //artist
+                console.log("Artist: " + songData.artists[0].name);
+                //song name
+                console.log("Song: " + songData.name);
+                //spotify preview link
+                console.log("Preview URL: " + songData.preview_url);
+                //album name
+                console.log("Album: " + songData.album.name);
+                console.log("-----------------------");
+
+
+            }
+        }
+    });
+    });
+}
+else {
+    console.log("atyare magaj ni nash na khench");
+}

3a0901b8bc43f1b8a7c714a425428618ac641525
purvam98 <35788384+purvam98@users.noreply.github.com>
2018-04-21 09:02:42 -0400

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,25 +1,28 @@
 require("dotenv").config();
+//Grab data from keys.js
 var keyfile = require("./keys.js");
-//var spotify = new Spotify(keys.spotify);
 var Twitter = require('twitter');
 var client = new Twitter(keyfile.twitter);
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keyfile.spotify);
 var request = require('request');
 var fs = require('fs');
-var allarg=process.argv;
-var final="";
-for(var m=3;m<allarg.length;m++)
-{
-    if(m>3 && m<allarg.length){
-        final = final + "+" + allarg[i];
-      } else{
-        final = final + allarg[i];
-      }
+//Stored argument's array
+var allarg = process.argv;
+//movie or song
+var final = "";
+//attaches multiple word arguments
+for (var m = 3; m < allarg.length; m++) {
+    if (m > 3 && m < allarg.length) {
+        final = final + "+" + allarg[m];
+    } else {
+        final = final + allarg[m];
+    }
 }
 var arg = process.argv[2];
-
+//if-else statement
 if (arg === "my-tweets") {
+    //Display last 20 Tweets
     var params = { screen_name: '@ChokshiPurvam' };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
@@ -33,36 +36,12 @@ if (arg === "my-tweets") {
 
 }
 else if (arg === "spotify-this-song") {
-    if(process.argv[3])
-    {
-    spotify.search({ type: 'track', query: final }, function (err, data) {
-        if (!err) {
-            for (var i = 0; i < data.tracks.items.length; i++) {
-                var songData = data.tracks.items[i];
-                //artist
-                console.log("Artist: " + songData.artists[0].name);
-                //song name
-                console.log("Song: " + songData.name);
-                //spotify preview link
-                console.log("Preview URL: " + songData.preview_url);
-                //album name
-                console.log("Album: " + songData.album.name);
-                console.log("-----------------------");
-
-
-            }
-        }
-    });
-    }
-    else
-    {
-        spotify.search({ type: 'track', query: "The+Sign" }, function (err, data) {
+    if (process.argv[3]) {
+        spotify.search({ type: 'track', query: final }, function (err, data) {
             if (!err) {
                 for (var i = 0; i < data.tracks.items.length; i++) {
                     var songData = data.tracks.items[i];
                     //artist
-                    if(songData.artists[0].name==="Ace of Base"&&songData.name==="The Sign")
-                    {
                     console.log("Artist: " + songData.artists[0].name);
                     //song name
                     console.log("Song: " + songData.name);
@@ -71,78 +50,89 @@ else if (arg === "spotify-this-song") {
                     //album name
                     console.log("Album: " + songData.album.name);
                     console.log("-----------------------");
-                    }
-                    else{
-                        
-                    }
-    
-    
+
+
                 }
             }
         });
     }
-
-}
-else if(arg==="movie-this")
-{
-    
-    if(process.argv[3])
-    {
-     
-        var omdbURL = "http://www.omdbapi.com/?t="+final+"&y=&plot=short&apikey=trilogy";
-    
-        request(omdbURL, function (error, response, body){
-          if(!error && response.statusCode == 200){
-            var body = JSON.parse(body);
-               
-            console.log("Title: " + body.Title);
-            console.log("Release Year: " + body.Year);
-            console.log("IMdB Rating: " + body.imdbRating);
-            console.log("Country: " + body.Country);
-            console.log("Language: " + body.Language);
-            console.log("Plot: " + body.Plot);
-            console.log("Actors: " + body.Actors);
-            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-            console.log("Rotten Tomatoes URL: " + body.tomatoURL);
-          }
-    });
-}
-else
-{
-    console.log("-----------------------");
-    console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-    console.log("It's on Netflix!");
-
-
-
-}
-
-}
-else if(arg==="do-what-it-says")
-{
-    fs.readFile('random.txt', "utf8", function(error, data){
-    
-    var text = data.split(',');
-    spotify.search({ type: 'track', query: text[1] }, function (err, data) {
-        if (!err) {
-            for (var i = 0; i < data.tracks.items.length; i++) {
-                var songData = data.tracks.items[i];
-                //artist
-                console.log("Artist: " + songData.artists[0].name);
-                //song name
-                console.log("Song: " + songData.name);
-                //spotify preview link
-                console.log("Preview URL: " + songData.preview_url);
-                //album name
-                console.log("Album: " + songData.album.name);
-                console.log("-----------------------");
-
-
+    else {
+        spotify.search({ type: 'track', query: "The+Sign" }, function (err, data) {
+            if (!err) {
+                for (var i = 0; i < data.tracks.items.length; i++) {
+                    var songData = data.tracks.items[i];
+                    //artist
+                    if (songData.artists[0].name === "Ace of Base" && songData.name === "The Sign") {
+                        console.log("Artist: " + songData.artists[0].name);
+                        //song name
+                        console.log("Song: " + songData.name);
+                        //spotify preview link
+                        console.log("Preview URL: " + songData.preview_url);
+                        //album name
+                        console.log("Album: " + songData.album.name);
+                        console.log("-----------------------");
+                    }
+                    else {
+                        console.log("We didn't found that album");
+                    }
+                }
             }
-        }
-    });
+        });
+    }
+}
+else if (arg === "movie-this") {
+
+    if (process.argv[3]) {
+
+        var omdbURL = "http://www.omdbapi.com/?t=" + final + "&y=&plot=short&apikey=trilogy";
+
+        request(omdbURL, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var body = JSON.parse(body);
+
+                console.log("Title: " + body.Title);
+                console.log("Release Year: " + body.Year);
+                console.log("IMdB Rating: " + body.imdbRating);
+                console.log("Country: " + body.Country);
+                console.log("Language: " + body.Language);
+                console.log("Plot: " + body.Plot);
+                console.log("Actors: " + body.Actors);
+                console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+                console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+            }
+        });
+    }
+    else {
+        console.log("-----------------------");
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+        console.log("It's on Netflix!");
+    }
+
+}
+else if (arg === "do-what-it-says") {
+    fs.readFile('random.txt', "utf8", function (error, data) {
+
+        var text = data.split(',');
+        spotify.search({ type: 'track', query: text[1] }, function (err, data) {
+            if (!err) {
+                for (var i = 0; i < data.tracks.items.length; i++) {
+                    var songData = data.tracks.items[i];
+                    //artist
+                    console.log("Artist: " + songData.artists[0].name);
+                    //song name
+                    console.log("Song: " + songData.name);
+                    //spotify preview link
+                    console.log("Preview URL: " + songData.preview_url);
+                    //album name
+                    console.log("Album: " + songData.album.name);
+                    console.log("-----------------------");
+
+
+                }
+            }
+        });
     });
 }
 else {
-    console.log("atyare magaj ni nash na khench");
+    console.log("Something Went Wrong ! Please try again with new command");
 }
